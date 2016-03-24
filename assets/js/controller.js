ecdaaed2fa702709f038031157131fcd5ea56f6b
Marco Bassaletti <bassaletti@gmail.com>
2016-03-24 09:32:29 +0000

    Support for editing contacts using inline editor via directive

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -91,10 +91,29 @@ directive('gravatar', function(){
             scope.class = attrs.class;
         }
     };
-})
+}).
 directive('editable', function() {
 	return {
-		
+		restrict: 'AE',
+		templateUrl: 'assets/partials/editable.html',
+		scope: {
+            value: '=editable',
+            field: '@fieldType'
+        },
+        controller: function($scope){
+        	$scope.editor = {
+			    showing: false,
+			    value: $scope.value
+			};
+			$scope.toggleEditor = function(){
+			    $scope.editor.showing = !$scope.editor.showing;
+			};
+			$scope.field = ($scope.field) ? $scope.field : 'text';
+			$scope.save = function(){
+			    $scope.value = $scope.editor.value;
+			    $scope.toggleEditor();
+			};
+        }
 	};
 }).
 filter('paragraph', function(){
