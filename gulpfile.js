f64b86a1842ec743b8679fbe9319129c344ac075
Marco Bassaletti <bassaletti@gmail.com>
2016-04-20 07:08:53 +0000

    Reorganized js sources. Fixed missing minified js file included in sources. Reformatted file.

--- gulpfile.js
+++ gulpfile.js
@@ -5,37 +5,35 @@ var pkg = require('./package.json');
 var webserver = require('gulp-webserver');
 
 var paths = {
-    js: [
-        'app/vendor/jquery.js',
-        'app/vendor/bootstrap.js',
-        'app/vendor/angular.js',
-        'app/vendor/angular-animate.js',
-        'app/vendor/angular-route.js',
-        'app/vendor/angular-sanitize.js',
-        'app/vendor/angular-strap.js',
-        'app/vendor/angular-strap.tpl.min.js',
-        'app/app.js',
-        'app/services/ContactsService.js',
-        'app/controllers/AppController.js',
-        'app/controllers/IndexController.js',
-        'app/controllers/AddController.js',
-        'app/controllers/ContactController.js',
-        'app/controllers/DemoController.js'
-    ]
+  js: [
+    'app/vendor/angular.js',
+    'app/vendor/angular-animate.js',
+    'app/vendor/angular-route.js',
+    'app/vendor/angular-sanitize.js',
+    'app/vendor/angular-strap.js',
+    'app/vendor/angular-strap.tpl.js',
+    'app/app.js',
+    'app/services/ContactsService.js',
+    'app/controllers/AppController.js',
+    'app/controllers/IndexController.js',
+    'app/controllers/AddController.js',
+    'app/controllers/ContactController.js',
+    'app/controllers/DemoController.js'
+  ]
 };
 
-gulp.task('uglify', function(){
-    gulp.src(paths.js)
+gulp.task('uglify', function () {
+  gulp.src(paths.js)
     .pipe(concat('ContactsMgr.min.js'))
     .pipe(uglify())
-    .pipe(gulp.dest('assets/js/build'));
+    .pipe(gulp.dest('assets/js'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch(paths.js, ['uglify']);
+gulp.task('watch', function () {
+  gulp.watch(paths.js, ['uglify']);
 });
 
-gulp.task('webserver', function() {
+gulp.task('webserver', function () {
   gulp.src('./')
     .pipe(webserver({
       host: '0.0.0.0',
